ce44977743a7d6f2eee75b8d2246b6fe9204eff5
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-12-01 00:38:48 +0600

    add

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+import ErrorComponent from "@/components/ErrorComponent";
+import { Fragment } from "react";
+
+export default function NotFound() {
+  return (
+    <Fragment>
+      <ErrorComponent />
+    </Fragment>
+  );
+}
